996903bd97a629983c6c6af64a05660189b76086
Johnny <johnnylxa@gmail.com>
2018-08-01 01:42:13 +0700

    add env setup

--- cory-house/env-setup/gulpfile.js
+++ cory-house/env-setup/gulpfile.js
@@ -16,16 +16,16 @@ var config = {
 		html: './src/*.html',
 		js: './src/**/*.js',
 		css: [
-      		'node_modules/bootstrap/dist/css/bootstrap.min.css',
-      		'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
-    	],
+			'node_modules/bootstrap/dist/css/bootstrap.min.css',
+			'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+		],
 		dist: './dist',
 		mainJs: './src/main.js'
 	}
 }
 
 //Start a local development server
-gulp.task('connect', function() {
+gulp.task('connect', function () {
 	connect.server({
 		root: ['dist'],
 		port: config.port,
@@ -34,18 +34,18 @@ gulp.task('connect', function() {
 	});
 });
 
-gulp.task('open', ['connect'], function() {
+gulp.task('open', ['connect'], function () {
 	gulp.src('dist/index.html')
-		.pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/'}));
+		.pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/' }));
 });
 
-gulp.task('html', function() {
+gulp.task('html', function () {
 	gulp.src(config.paths.html)
 		.pipe(gulp.dest(config.paths.dist))
 		.pipe(connect.reload());
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
 	browserify(config.paths.mainJs)
 		.transform(reactify)
 		.bundle()
@@ -55,19 +55,19 @@ gulp.task('js', function() {
 		.pipe(connect.reload());
 });
 
-gulp.task('css', function() {
+gulp.task('css', function () {
 	gulp.src(config.paths.css)
 		.pipe(concat('bundle.css'))
 		.pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
-gulp.task('lint', function() {
+gulp.task('lint', function () {
 	return gulp.src(config.paths.js)
-		.pipe(lint({config: 'eslint.config.json'}))
+		.pipe(lint({ config: 'eslint.config.json' }))
 		.pipe(lint.format());
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
 	gulp.watch(config.paths.html, ['html']);
 	gulp.watch(config.paths.js, ['js', 'lint']);
 });
